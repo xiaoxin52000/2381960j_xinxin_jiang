ee0c5a5a72c31cabc025a833f63a62d93a9b029c
xinxin jiang <2381960j@student.gla.ac.uk>
2019-01-20 13:55:07 +0000

    chapter1-5.3

---
+++ rango/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookoe, candy, cupcake"}
+    return render(request, 'rango/index.html', context=context_dict)
