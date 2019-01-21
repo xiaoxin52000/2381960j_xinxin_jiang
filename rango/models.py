3255b26a7d328ab92c3d1b00dafaa7723947e0a2
xinxin jiang <2381960j@student.gla.ac.uk>
2019-01-21 14:56:52 +0000

    chapter 1-5

--- rango/models.py
+++ rango/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
-    def _str_(self):
+    class Meta:
+         verbose_name_plural = 'Categories'
+    def __str__(self): 
         return self.name
 
 class Page(models.Model):
-    category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
+    category = models.ForeignKey(Category) 
+    title = models.CharField(max_length=128) 
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def _str_(self):
+    def __str__(self): 
         return self.title
+      
--- rango/views.py
+++ rango/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookoe, candy, cupcake"}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Rango says hey there partner")
