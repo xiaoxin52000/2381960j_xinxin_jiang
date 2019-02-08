ec7f79c076538798f9993880c7c77d18a5e91922
xinxin jiang <2381960j@student.gla.ac.uk>
2019-02-08 22:11:07 +0000

    chapter 1-10

--- rango/views.py
+++ rango/views.py
@@ -76,12 +76,12 @@ def index(request):
     return response
 
 def about(request):
-    '''if request.session.test_cookie_worked(): 
+    if request.session.test_cookie_worked(): 
         print("TEST COOKIE WORKED!") 
         request.session.delete_test_cookie()
     print(request.method)
     print(request.user)
-    
+    '''
     return render(request,'rango/about.html')
     '''
     context_dict={}
@@ -103,7 +103,7 @@ def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request,'visits','1'))
     last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
-    if(datetime.now() - last_visit_time).seconds>0:
+    if(datetime.now() - last_visit_time).days>0:
         visits = visits + 1
         request.session['last_visit'] = str(datetime.now())
     else:
